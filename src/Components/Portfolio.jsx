f64c92f7bcfb241f840543113f52013c1388b763
Elisha Kiplangat <102684837+Elisha-Kiplangat@users.noreply.github.com>
2023-10-09 07:17:37 +0000

    Added my details

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -31,28 +31,28 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Web Development",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Developed a simple Login form using HTML, CSS and JAVASCRIPT",
+    url: "https://github.com/Elisha-Kiplangat/login-form",
   },
   {
-    title: "Web Development for Beginners",
+    title: "Azure Static Web Apps 🎉",
     description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+      "Explore my skills with the Microsoft Azure Static Web Apps.",
+    url: "",
   },
   {
     title: "My Resume Site",
     description:
       "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
+    url: "",
   },
   {
-    title: "GitHub Codespaces and github.dev",
+    title: "GitHub Codespaces",
     description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Currenty learning on the Github codespaces and all the features it offer for web develoopment.",
+    url: "",
   },
 ];
 
